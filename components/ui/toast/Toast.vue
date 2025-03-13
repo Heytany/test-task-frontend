35f3072614330813c71d78e54705d7f21bd34c27
Alexsey Artemiev <artemiev@progressivemedia.ru>
2025-03-13 15:46:50 +0300

    Update dependency
    fix useForward from lib
    
    Update dependency

--- components/ui/toast/Toast.vue
+++ components/ui/toast/Toast.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import type { ToastRootEmits } from 'reka-ui'
-import { ToastRoot } from 'reka-ui'
+import { useForwardPropsEmits, ToastRoot } from 'reka-ui'
+
 import { computed } from 'vue'
 import type { ToastProps } from '.'
 import { toastVariants } from '.'
-import { useForwardPropsEmits } from '~/utils/useForward'
 import { cn } from '~/utils/utils'
 
 const props = defineProps<ToastProps>()
